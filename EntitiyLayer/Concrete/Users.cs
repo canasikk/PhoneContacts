4998d6ba45546afe956a4ee265c32905f7512239
Can Aşık <qratipum@gmail.com>
2023-08-05 15:06:29 +0300

    Add project files.

---
+++ EntitiyLayer/Concrete/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyLayer.Concrete
+{
+    public class Users
+    {
+        public int UUID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public int PhoneNumber { get; set; }
+        public string EMail { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ PhoneContacts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhoneContacts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
